b0b9a6976453027a38759ceed52d1851dc51decd
Chris Gagnier <mcgagnier@yahoo.com>
2018-01-27 13:51:56 -0700

    done

--- src/components/Topics/FilterString.js
+++ src/components/Topics/FilterString.js
@@ -1,9 +1,40 @@
 import React, { Component } from 'react';
 
 export default class FilterString extends Component {
+    constructor(){
+        super();
+
+        this.state ={
+            cars: ['vette', 'camaro', 'bmw', 'prius', 'element'],
+            userInput: '',
+            filteredCars: []
+        }
+    }
+    handleChange(val) {
+        this.setState({ userInput: val});
+    }
+    filterNames(userInput){
+        var cars = this.state.cars;
+        var filteredCars = [];
+        for(var i=0;i<cars.length;i++){
+            if(cars[i].includes( userInput ) ){
+                console.log(cars[i]);
+                filteredCars.push(cars[i]);
+            }
+        }
+        console.log(filteredCars, 'zzzzz');
+        this.setState({filteredCars: filteredCars});
+    }
+
         render() {
             return (
-                <p> Filter Object Component </p>
+                <div className="puzzleBox filterStringPB">
+                    <h4>Filter String</h4>
+                    <span className="puzzleText">Cars: { JSON.stringify(this.state.cars, null, 10)}</span>
+                    <input className="inputLine" onChange={ (e) => this.handleChange(e.target.value)}></input>
+                    <button className="confirmationButton" onClick={ () => this.filterNames(this.state.userInput)}>Find it</button>
+                    <span className="resultsBox filterStringRB">{JSON.stringify(this.state.filteredCars, null, 10)}</span>
+                </div>
             )
         }
     }
--- src/components/Topics/Palindrome.js
+++ src/components/Topics/Palindrome.js
@@ -1,9 +1,44 @@
 import React, { Component } from 'react';
 
 export default class Palindrome extends Component {
+    constructor() {
+        super()
+
+        this.state={
+            userInput: '',
+            palindrome: ''
+        }
+    }
+    handleChange(val){
+        this.setState({userInput: val});
+    }
+    isPalindrome(userInput){
+        var forward = userInput;
+        var backwards = userInput;
+        backwards = backwards.split('');
+        backwards = backwards.reverse();
+        backwards = backwards.join('');
+        console.log(backwards);
+        if(forward === backwards){
+            console.log('yep')
+            this.setState({ palindrome: 'true'});
+        } else {
+            console.log("nope")
+            this.setState({ palindrome: 'false'});
+        }
+        console.log("iiiiiii", this.state.palindrome)
+    }
+
         render() {
             return (
-                <p> Palendrome Component </p>
+                <div className="puzzleBox filterStringPB">
+                    <h4>Palindrome</h4>
+                    <input className="inputLine" onChange={ (e) => this.handleChange(e.target.value) }></input>
+                    <button className="confirmationButton" 
+                    onClick = { ()=> this.isPalindrome(this.state.userInput)}>Test</button>
+                    <span className="resultsBox">
+                    Palindrome: {console.log("here", this.state.palindrome)}{ this.state.palindrome }</span>
+                </div>
             )
         }
     }
